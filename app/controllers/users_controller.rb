5d6a6f64995cc77c2de12e2c1124bf0b19b39db7
Rio-cyber-gif <229548725+Rio-cyber-gif@users.noreply.github.com>
2026-04-24 11:39:55 +0900

    公開プロフィールページを追加: 投稿者アイコンクリックでユーザー情報と投稿一覧を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:mypage]
+
+  def show
+    @user = User.find(params[:id])
+    @specialties = @user.specialties
+                        .publicly_visible
+                        .includes(:region, :favorites, :tags)
+                        .order(created_at: :desc)
+                        .page(params[:page]).per(9)
+  end
 
   def mypage
     @my_specialties = current_user.specialties
