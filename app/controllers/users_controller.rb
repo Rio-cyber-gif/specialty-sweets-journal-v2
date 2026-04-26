537c8585441c21ad47c5df6a26fef342e1ab7921
Rio-cyber-gif <229548725+Rio-cyber-gif@users.noreply.github.com>
2026-04-26 12:50:35 +0900

    プロフィール編集フォームを2つに分割する
    アイコン・名前・自己紹介はパスワード不要で保存できる上段フォームに、
    メールアドレス・パスワード変更は現在のパスワード必須の下段フォームに分離。
    画像選択後にパスワードエラーが出ても再選択不要になる。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:mypage]
+  before_action :authenticate_user!, only: [:mypage, :update_profile]
+
+  private
+
+  def profile_params
+    params.require(:user).permit(:name, :bio, :avatar, :remove_avatar)
+  end
+
+  public
 
   def show
     @user = User.find(params[:id])
@@ -10,6 +18,17 @@ class UsersController < ApplicationController
                         .includes(:region, :favorites, :tags)
                         .order(created_at: :desc)
                         .page(params[:page]).per(9)
+  end
+
+  def update_profile
+    current_user.avatar.purge if params[:user][:remove_avatar] == '1' && current_user.avatar.attached?
+    params[:user].delete(:avatar) if params[:user][:avatar].blank?
+
+    if current_user.update(profile_params)
+      redirect_to edit_user_registration_path, success: 'プロフィールを更新しました'
+    else
+      redirect_to edit_user_registration_path, danger: current_user.errors.full_messages.join('、')
+    end
   end
 
   def mypage
